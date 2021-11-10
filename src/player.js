d59b2c2f63707331ed0215a12720656018b99063
Rosa The Dev <richardjrosaserrano@gmail.com>
2021-11-10 16:10:25 -0500

    Create a player class with the respective constructors and mentods

---
+++ src/player.js
@@ -0,0 +1,20 @@
+class Player {
+    constructor(human, token, wins) {
+        this.human = human;
+        this.token = token;
+        this.wins = wins || 0;
+    }
+
+    saveWinsToStorage() {
+
+    }
+
+    retrieveWinsFromStorage() {
+
+    }
+
+    takeTurn() {
+        
+    }
+}
+module.exports = Player;
